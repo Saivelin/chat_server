247c52f0649cff2281318978aeb0b3d1277e04bf
Saivelin <vlad.pochkalov@mail.ru>
2024-07-02 01:23:30 +0300

    file upload

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { extname } from 'path';
+import { diskStorage } from 'multer';
 
 @Controller()
 export class AppController {
@@ -9,4 +12,28 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Post('upload')
+    @UseInterceptors(FileInterceptor('file', {
+        storage: diskStorage({
+            destination: './uploads',
+            filename: (req, file, cb) => {
+                const randomName = Array(32).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('')
+                cb(null, `${randomName}${extname(file.originalname)}`)
+            }
+        })
+    }))
+    uploadFile(@UploadedFile() file: any) {
+        const oneKb = 1024
+        const oneMb = oneKb * 1024
+        if(file.size > (oneMb * 10)){
+            return `File is too big to load`
+        }
+        return file.filename
+    }
+
+    @Get('upload/:filename')
+    async getFile(@Param("filename") filename: string, @Res() res: any){
+        res.sendFile(filename, {root:'uploads'})
+    }
 }
